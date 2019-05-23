185b7ee6f8eededaa1d60b143f56f7d6421a51ed
Martin Klingman <martinklingman@Martins-MacBook-Pro.local>
2019-05-23 11:57:26 +0200

    trying to add upload image function

--- frontier.js
+++ frontier.js
@@ -1,1 +1,23 @@
-javascript
+const image = document.querySelectorAll("img");
+
+image.forEach(addImg);
+
+function addImg(img) {
+	img.addEventListener("click", imgClicked);
+}
+let count = 0
+
+function addChange(img) {
+	img.classList.add("changeit");
+}
+
+function imgClicked() {
+	count++
+	console.log(this)
+	this.classList.add("fade");
+	this.removeEventListener("click", imgClicked);
+	if (count == 3) {
+		image.forEach(addChange);
+	}
+
+}
